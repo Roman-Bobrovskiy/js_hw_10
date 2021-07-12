8be8deb94721340086bb7d90d426499d25ecb88a
RB <bobrovskiyroman@gmail.com>
2021-07-12 18:10:26 +0300

    task3

--- js/task3.js
+++ js/task3.js
@@ -1,34 +1,34 @@
-let date = document.querySelector('p')
-let ldate = document.querySelector('#localdate')
-ldate.addEventListener("input",sDate)
+let day = document.querySelector('span[data-value="days"]');
+let hour = document.querySelector('span[data-value="hours"]');
+let min = document.querySelector('span[data-value="mins"]');
+let sec = document.querySelector('span[data-value="secs"]');
+let p = document.querySelector("p");
+let dateOfStart = document.querySelector("#startDate");
+dateOfStart.addEventListener("input", sDate);
+console.log(dateOfStart.value);
 
 function sDate() {
+  let promotionTimer = new Date(dateOfStart.value);
+  console.log(promotionTimer);
 
-let p = document.querySelector("p");
-console.log(p);
-
-
- let promotionTimer = new Date(ldate.value);
-console.log(promotionTimer);   
-
-
-
-let promotonTimerId = setInterval(() => {
-    let dateNow = Date.now(); // timestamp
-    // console.log(dateNow);
-
+  let promotonTimerId = setInterval(() => {
+    let dateNow = Date.now();
     let distance = promotionTimer - dateNow;
     let days = Math.floor(distance / (1000 * 60 * 60 * 24));
     let hours = Math.floor(
-        (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+      (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
     );
     let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
     let seconds = Math.floor((distance % (1000 * 60)) / 1000);
-
-    p.innerHTML = `Days: ${days}, Hours: ${hours}, Minutes: ${minutes}, Seconds: ${seconds}`;
+    day.innerHTML = days;
+    hour.innerHTML = hours;
+    min.innerHTML = minutes;
+    sec.innerHTML = seconds;
+    clearInterval(promotonTimerId);
 
     if (distance < 0) {
-        clearInterval(promotonTimerId);
-        p.innerHTML = "Акция закончилась";
+      clearInterval(promotonTimerId);
+      p.innerHTML = "Time is over";
     }
-}, 1000);}
+  }, 1000);
+}
