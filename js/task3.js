9efc8dff1b63dea0ac43c853ed7ff769d062dcba
RB <bobrovskiyroman@gmail.com>
2021-07-13 18:56:01 +0300

    task3_v3

--- js/task3.js
+++ js/task3.js
@@ -1,15 +1,29 @@
 class Timer {
   constructor() {
+    console.log("const");
     this.day = document.querySelector('span[data-value="days"]');
     this.hour = document.querySelector('span[data-value="hours"]');
     this.min = document.querySelector('span[data-value="mins"]');
     this.sec = document.querySelector('span[data-value="secs"]');
     this.p = document.querySelector("p");
+    this.input = document.querySelector("#startDate");
+    this.init();
+    this.promotonTimerId = null;
+  }
+  init() {
+    console.log("init()");
+    this.input.addEventListener("input", this.sTime.bind(this));
   }
 
   sTime() {
-    let promotionTimer = new Date("Mar 20, 2022 13:00:00");
-    let promotonTimerId = setInterval(() => {
+    console.log("sTime()", this.input);
+    if (this.promotonTimerId !== null) {
+      clearInterval(this.promotonTimerId);
+      this.promotonTimerId = null;
+    }
+    let promotionTimer = new Date(this.input.value);
+    console.log(promotionTimer);
+    this.promotonTimerId = setInterval(() => {
       let dateNow = Date.now();
       let distance = promotionTimer - dateNow;
       let days = Math.floor(distance / (1000 * 60 * 60 * 24));
@@ -18,16 +32,18 @@ class Timer {
       );
       let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
       let seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
       this.day.innerHTML = days;
       this.hour.innerHTML = hours;
       this.min.innerHTML = minutes;
       this.sec.innerHTML = seconds;
       if (distance < 0) {
-        clearInterval(promotonTimerId);
+        clearInterval(this.promotonTimerId);
+        this.promotonTimerId = null;
         this.p.innerHTML = "Time is over";
       }
     }, 1000);
   }
 }
 
-new Timer().sTime();
+new Timer();
