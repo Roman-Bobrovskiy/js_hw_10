6a03714f1a095f9f8a0d274816bf02d87fa717b4
RB <bobrovskiyroman@gmail.com>
2021-07-13 11:58:41 +0300

    task3

--- js/task3.js
+++ js/task3.js
@@ -1,34 +1,33 @@
-let day = document.querySelector('span[data-value="days"]');
-let hour = document.querySelector('span[data-value="hours"]');
-let min = document.querySelector('span[data-value="mins"]');
-let sec = document.querySelector('span[data-value="secs"]');
-let p = document.querySelector("p");
-let dateOfStart = document.querySelector("#startDate");
-dateOfStart.addEventListener("input", sDate);
-console.log(dateOfStart.value);
+class Timer {
+  constructor() {
+    this.day = document.querySelector('span[data-value="days"]');
+    this.hour = document.querySelector('span[data-value="hours"]');
+    this.min = document.querySelector('span[data-value="mins"]');
+    this.sec = document.querySelector('span[data-value="secs"]');
+    this.p = document.querySelector("p");
+  }
 
-function sDate() {
-  let promotionTimer = new Date(dateOfStart.value);
-  console.log(promotionTimer);
-
-  let promotonTimerId = setInterval(() => {
-    let dateNow = Date.now();
-    let distance = promotionTimer - dateNow;
-    let days = Math.floor(distance / (1000 * 60 * 60 * 24));
-    let hours = Math.floor(
-      (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
-    );
-    let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-    let seconds = Math.floor((distance % (1000 * 60)) / 1000);
-    day.innerHTML = days;
-    hour.innerHTML = hours;
-    min.innerHTML = minutes;
-    sec.innerHTML = seconds;
-    clearInterval(promotonTimerId);
-
-    if (distance < 0) {
-      clearInterval(promotonTimerId);
-      p.innerHTML = "Time is over";
-    }
-  }, 1000);
+  sTime() {
+    let promotionTimer = new Date("Mar 20, 2022 13:00:00");
+    let promotonTimerId = setInterval(() => {
+      let dateNow = Date.now();
+      let distance = promotionTimer - dateNow;
+      let days = Math.floor(distance / (1000 * 60 * 60 * 24));
+      let hours = Math.floor(
+        (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+      );
+      let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+      let seconds = Math.floor((distance % (1000 * 60)) / 1000);
+      this.day.innerHTML = days;
+      this.hour.innerHTML = hours;
+      this.min.innerHTML = minutes;
+      this.sec.innerHTML = seconds;
+      if (distance < 0) {
+        clearInterval(promotonTimerId);
+        this.p.innerHTML = "Time is over";
+      }
+    }, 1000);
+  }
 }
+
+new Timer().sTime();
